4b0b71dce5cf325dc43356f02a57c03e31720e51
imgaurav-17 <123211146+imgaurav-17@users.noreply.github.com>
2024-11-21 12:38:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 10000;
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+});
 import { Telegraf } from 'telegraf';
 import axios from 'axios';
 import fs from 'fs';
